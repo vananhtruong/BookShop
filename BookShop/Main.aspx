a042dc80bcbd6a1ca3365863319a63c737d7e374
vananh truong <truongvananh456@gmail.com>
2024-09-25 07:12:27 +0700

    screen books foruser and session to add to card

---
+++ BookShop/Main.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Main.aspx.cs" Inherits="BookShop.Main" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="txtSearch">Search by Title or Author:</label>
+                    <asp:TextBox ID="txtSearch" runat="server" CssClass="form-control" />
+                </div>
+
+                <div class="form-group">
+                    <label for="ddlCategory">Filter by Category:</label>
+                    <asp:DropDownList ID="ddlCategory" runat="server" CssClass="form-control" AutoPostBack="true" OnSelectedIndexChanged="ddlCategory_SelectedIndexChanged">
+                        <asp:ListItem Text="All Categories" Value="0"></asp:ListItem>
+                    </asp:DropDownList>
+                </div>
+
+                <asp:Button ID="btnSearch" runat="server" CssClass="btn btn-primary" Text="Search" OnClick="btnSearch_Click" />
+            </div>
+
+            <div class="col-md-9">
+                <asp:GridView ID="gvProducts" runat="server" CssClass="table table-striped" AutoGenerateColumns="False">
+                    <Columns>
+                        <asp:BoundField DataField="Title" HeaderText="Book Title" />
+                        <asp:BoundField DataField="Author" HeaderText="Author" />
+                        <asp:BoundField DataField="CategoryName" HeaderText="Category" />
+                        <asp:BoundField DataField="Price" HeaderText="Price" DataFormatString="{0:C}" />
+
+                        <asp:TemplateField>
+                            <ItemTemplate>
+                                <asp:Button ID="btnDetail" runat="server" Text="Detail" CssClass="btn btn-info" CommandArgument='<%# Eval("BookID") %>' OnClick="btnDetail_Click" />
+                                <asp:Button ID="btnAddToCart" runat="server" Text="Add to Cart" CssClass="btn btn-success"
+                                    CommandArgument='<%# Eval("BookID") + ";" + Eval("Title") + ";" + Eval("Price") %>'
+                                    OnClick="btnAddToCart_Click" />
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+                </asp:GridView>
+
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ BookShop/Main.aspx.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BookShop
+{
+    public partial class Main : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindCategories();
+                BindProducts();
+            }
+        }
+
+        // Method to bind categories to dropdown
+        private void BindCategories()
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnectionString"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT CategoryID, CategoryName FROM BookCategory WHERE IsDeleted = 0", conn);
+                conn.Open();
+                ddlCategory.DataSource = cmd.ExecuteReader();
+                ddlCategory.DataTextField = "CategoryName";
+                ddlCategory.DataValueField = "CategoryID";
+                ddlCategory.DataBind();
+            }
+            ddlCategory.Items.Insert(0, new ListItem("All Categories", "0"));
+        }
+
+        // Method to bind products to GridView with search and filter
+        private void BindProducts(string searchKeyword = "", int categoryId = 0)
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnectionString"].ConnectionString))
+            {
+                string query = "SELECT b.BookID, b.Title, b.Author, b.Price, c.CategoryName " +
+                               "FROM Book b " +
+                               "INNER JOIN BookCategory c ON b.CategoryID = c.CategoryID " +
+                               "WHERE b.IsDeleted = 0 AND " +
+                               "(@CategoryID = 0 OR b.CategoryID = @CategoryID) " +
+                               "AND (b.Title LIKE @SearchKeyword OR b.Author LIKE @SearchKeyword) " +
+                               "ORDER BY b.Title";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@CategoryID", categoryId);
+                cmd.Parameters.AddWithValue("@SearchKeyword", "%" + searchKeyword + "%");
+                conn.Open();
+
+                gvProducts.DataSource = cmd.ExecuteReader();
+                gvProducts.DataBind();
+            }
+        }
+
+        // Handle search button click
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchKeyword = txtSearch.Text.Trim();
+            int categoryId = int.Parse(ddlCategory.SelectedValue);
+            BindProducts(searchKeyword, categoryId);
+        }
+
+        // Handle category filter change
+        protected void ddlCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string searchKeyword = txtSearch.Text.Trim();
+            int categoryId = int.Parse(ddlCategory.SelectedValue);
+            BindProducts(searchKeyword, categoryId);
+        }
+
+        // Handle Detail button click
+        protected void btnDetail_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int bookId = Convert.ToInt32(btn.CommandArgument);
+            // Redirect to detail page or load detail information
+            Response.Redirect("BookDetail.aspx?BookID=" + bookId);
+        }
+
+        // Handle Add to Cart button click
+        protected void btnAddToCart_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            // Tách dữ liệu từ CommandArgument (BookID, Title, Price)
+            string[] commandArgs = btn.CommandArgument.Split(';');
+            int bookId = Convert.ToInt32(commandArgs[0]);
+            string title = commandArgs[1];
+            decimal price = Convert.ToDecimal(commandArgs[2]);
+
+            // Lấy giỏ hàng từ session hoặc khởi tạo mới nếu chưa có
+            DataTable cart = Session["Cart"] as DataTable;
+            if (cart == null)
+            {
+                cart = new DataTable();
+                cart.Columns.Add("BookID", typeof(int));
+                cart.Columns.Add("Title", typeof(string));
+                cart.Columns.Add("Price", typeof(decimal));
+                cart.Columns.Add("Quantity", typeof(int));
+            }
+
+            // Thêm sách vào giỏ hàng hoặc tăng số lượng nếu đã tồn tại
+            DataRow[] existingRows = cart.Select("BookID = " + bookId);
+            if (existingRows.Length > 0)
+            {
+                existingRows[0]["Quantity"] = (int)existingRows[0]["Quantity"] + 1;
+            }
+            else
+            {
+                DataRow row = cart.NewRow();
+                row["BookID"] = bookId;
+                row["Title"] = title;
+                row["Price"] = price;
+                row["Quantity"] = 1;
+                cart.Rows.Add(row);
+            }
+
+            // Lưu giỏ hàng vào session
+            Session["Cart"] = cart;
+        }
+
+    }
+}
